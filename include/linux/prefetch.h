2a5984135e71d4a23a16d57346f554fc15614c4c
chaoyu <chaoyu9225@gmail.com>
2025-09-26 18:29:17 +0800

    .

--- include/linux/prefetch.h
+++ include/linux/prefetch.h
@@ -1,5 +1,5 @@
 /*
- *  Generic cache management functions. Everything is arch-specific,  
+ *  Generic cache management functions. Everything is arch-specific,
  *  but this header exists to make sure the defines/functions can be
  *  used in a generic way.
  *
@@ -16,29 +16,31 @@
 
 /*
 	prefetch(x) attempts to pre-emptively get the memory pointed to
-	by address "x" into the CPU L1 cache. 
+	by address "x" into the CPU L1 cache.
 	prefetch(x) should not cause any kind of exception, prefetch(0) is
 	specifically ok.
 
-	prefetch() should be defined by the architecture, if not, the 
-	#define below provides a no-op define.	
-	
+	prefetch() should be defined by the architecture, if not, the
+	#define below provides a no-op define.
+
 	There are 3 prefetch() macros:
-	
+
 	prefetch(x)  	- prefetches the cacheline at "x" for read
 	prefetchw(x)	- prefetches the cacheline at "x" for write
 	spin_lock_prefetch(x) - prefectches the spinlock *x for taking
-	
-	there is also PREFETCH_STRIDE which is the architecure-prefered 
+
+	虽然都是缓存，但是MESI缓存状态不同，所以此处需要区分预读、预写.
+
+	there is also PREFETCH_STRIDE which is the architecure-prefered
 	"lookahead" size for prefetching streamed operations.
-	
+
 */
 
 /*
  *	These cannot be do{}while(0) macros. See the mental gymnastics in
  *	the loop macro.
  */
- 
+
 #ifndef ARCH_HAS_PREFETCH
 static inline void prefetch(const void *x) {;}
 #endif
