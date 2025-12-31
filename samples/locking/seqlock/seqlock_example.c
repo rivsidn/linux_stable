ca58c02d852be33a1a4336dc934ba04ba8c5860c
chaoyu <chaoyu9225@gmail.com>
2025-12-31 14:45:10 +0800

    实现seqlock_example.c 代码，熟悉seqlock 函数接口.

--- samples/locking/seqlock/seqlock_example.c
+++ samples/locking/seqlock/seqlock_example.c
@@ -1,2 +1,37 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
 
+#include <linux/seqlock.h>
 
+static seqcount_t seq_test = SEQCNT_ZERO(seq_test);
+
+static int __init seqlock_demo_init(void)
+{
+	int i;
+
+	pr_info("%s\n", __func__);
+
+	for (i = 0; i < 3; i++) {
+		write_seqcount_begin(&seq_test);
+		pr_info("%d\n", seq_test.sequence);
+		write_seqcount_end(&seq_test);
+		pr_info("%d\n", seq_test.sequence);
+	}
+
+	return 0;
+}
+
+static void __exit seqlock_demo_exit(void)
+{
+	pr_info("%s\n", __func__);
+	return;
+}
+
+module_init(seqlock_demo_init);
+module_exit(seqlock_demo_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("rivsidn");
+MODULE_DESCRIPTION("seqlock examples");
+MODULE_VERSION("1.0");
