27e64b867a5fc99ff391bbec064a06214063c634
chaoyu <chaoyu9225@gmail.com>
2026-01-15 11:43:08 +0800

    proc文件系统示例: 构建dir_0、dir_1 目录.

--- samples/proc/proc_samples.c
+++ samples/proc/proc_samples.c
@@ -6,18 +6,18 @@
  * /proc/samples/
  * │
  * ├── dir_0/
- * │   ├── node0
- * │   ├── node1
- * │   └── node2
+ * │   ├── node_0
+ * │   ├── node_1
+ * │   └── node_2
  * │
  * ├── dir_1/
- * │   ├── node0
- * │   ├── node1
- * │   └── node2
+ * │   ├── node_0
+ * │   ├── node_1
+ * │   └── node_2
  * │
  * └── dir_2/
  *     └── subdir_0
- *            └── node0
+ *            └── node_0
  *
  */
 
@@ -29,8 +29,96 @@
 
 static struct proc_dir_entry *samples_dir;
 static struct proc_dir_entry *dir_0;
+static struct proc_dir_entry *dir_0_node_0;
+static struct proc_dir_entry *dir_0_node_1;
+static struct proc_dir_entry *dir_0_node_2;
 static struct proc_dir_entry *dir_1;
+static struct proc_dir_entry *dir_1_node_0;
+static struct proc_dir_entry *dir_1_node_1;
+static struct proc_dir_entry *dir_1_node_2;
 static struct proc_dir_entry *dir_2;
+
+static const struct file_operations node_ops = {
+	.owner		= THIS_MODULE,
+	.open		= NULL,
+	.read		= NULL,
+	.llseek		= NULL,
+	.release	= NULL,
+};
+
+static int proc_build_dir_0(void)
+{
+	int ret = -ENOMEM;
+
+	dir_0_node_0 = proc_create("node_0", 0, dir_0, &node_ops);
+	if (!dir_0_node_0) {
+		pr_err("dir_0 failed to create node_0\n");
+		goto err_node_0;
+	}
+	dir_0_node_1 = proc_create("node_1", 0, dir_0, &node_ops);
+	if (!dir_0_node_1) {
+		pr_err("dir_0 failed to create node_1\n");
+		goto err_node_1;
+	}
+	dir_0_node_2 = proc_create("node_2", 0, dir_0, &node_ops);
+	if (!dir_0_node_2) {
+		pr_err("dir_0 failed to create node_2\n");
+		goto err_node_2;
+	}
+
+	return 0;
+
+err_node_2:
+	remove_proc_entry("node_1", dir_0);
+err_node_1:
+	remove_proc_entry("node_0", dir_0);
+err_node_0:
+	return ret;
+}
+
+static void proc_clean_dir_0(void)
+{
+	remove_proc_entry("node_2", dir_0);
+	remove_proc_entry("node_1", dir_0);
+	remove_proc_entry("node_0", dir_0);
+}
+
+static int proc_build_dir_1(void)
+{
+	int ret = -ENOMEM;
+
+	dir_1_node_0 = proc_create("node_0", 0, dir_1, &node_ops);
+	if (!dir_1_node_0) {
+		pr_err("dir_1 failed to create node_0\n");
+		goto err_node_0;
+	}
+	dir_1_node_1 = proc_create("node_1", 0, dir_1, &node_ops);
+	if (!dir_1_node_1) {
+		pr_err("dir_1 failed to create node_1\n");
+		goto err_node_1;
+	}
+	dir_1_node_2 = proc_create("node_2", 0, dir_1, &node_ops);
+	if (!dir_1_node_2) {
+		pr_err("dir_1 failed to create node_2\n");
+		goto err_node_2;
+	}
+
+	return 0;
+
+err_node_2:
+	remove_proc_entry("node_1", dir_1);
+err_node_1:
+	remove_proc_entry("node_0", dir_1);
+err_node_0:
+	return ret;
+}
+
+static void proc_clean_dir_1(void)
+{
+	remove_proc_entry("node_2", dir_1);
+	remove_proc_entry("node_1", dir_1);
+	remove_proc_entry("node_0", dir_1);
+}
 
 static int __init proc_demo_init(void)
 {
@@ -47,11 +135,25 @@ static int __init proc_demo_init(void)
 		pr_err("failed to create proc dir_0\n");
 		goto err_dir_0;
 	}
+	ret = proc_build_dir_0();
+	if (ret) {
+		pr_err("failed to build proc dir_0\n");
+		goto err_build_dir_0;
+	}
+
+	ret = -ENOMEM;
 	dir_1 = proc_mkdir("dir_1", samples_dir);
 	if (!dir_1) {
 		pr_err("failed to create proc dir_1\n");
 		goto err_dir_1;
 	}
+	ret = proc_build_dir_1();
+	if (ret) {
+		pr_err("failed to build proc dir_1\n");
+		goto err_build_dir_1;
+	}
+
+	ret = -ENOMEM;
 	dir_2 = proc_mkdir("dir_2", samples_dir);
 	if (!dir_2) {
 		pr_err("failed to create proc dir_2\n");
@@ -61,8 +163,12 @@ static int __init proc_demo_init(void)
 	return 0;
 
 err_dir_2:
+	proc_clean_dir_1();
+err_build_dir_1:
 	remove_proc_entry("dir_1", samples_dir);
 err_dir_1:
+	proc_clean_dir_0();
+err_build_dir_0:
 	remove_proc_entry("dir_0", samples_dir);
 err_dir_0:
 	remove_proc_entry("samples", NULL);
@@ -73,8 +179,13 @@ err_samples:
 static void __exit proc_demo_exit(void)
 {
 	remove_proc_entry("dir_2", samples_dir);
+
+	proc_clean_dir_1();
 	remove_proc_entry("dir_1", samples_dir);
+
+	proc_clean_dir_0();
 	remove_proc_entry("dir_0", samples_dir);
+
 	remove_proc_entry("samples", NULL);
 
 	return;
