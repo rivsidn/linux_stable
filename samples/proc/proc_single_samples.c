e4152e5722c4aeed6717d9f8e97dd8b82d676964
chaoyu <chaoyu9225@gmail.com>
2026-01-16 11:41:53 +0800

    proc文件系统示例: 实现一个最简单的proc 文件示例.

--- samples/proc/proc_single_samples.c
+++ samples/proc/proc_single_samples.c
@@ -9,7 +9,11 @@
 #include <linux/init.h>
 
 #include <linux/proc_fs.h>
+#include <linux/uaccess.h>
 #include <linux/seq_file.h>
+
+#include <linux/printk.h>
+
 
 static struct proc_dir_entry *samples_node;
 
@@ -26,16 +30,49 @@ static const struct file_operations node_ops = {
 	.release	= NULL,
 };
 
+/*
+ * 即使没有open 操作，读写也可以正常执行.
+ * 所以这里的open 操作更像是一个hook点，允许用户在open 的时候执行具体的动作.
+ */
+/* 文件描述符是上层维护的，这里的返回值和文件描述符没有关系. */
 static int node_open(struct inode *inode, struct file *file)
 {
+	dump_stack();
+
+	/* 返回0 表示成功 */
 	return 0;
 }
-static ssize_t node_read(struct file *file, char __user *buffer, size_t count, loff_t *offset)
+static ssize_t node_read(struct file *file, char __user *buffer, size_t usize, loff_t *offset)
 {
-	return 0;
+	int ret;
+	int size = sizeof("aa\n");
+
+	if (*offset != 0) {
+		return 0;
+	}
+
+	ret = copy_to_user(buffer, "aa\n", size);
+
+	/* 需要手动更新offset */
+	*offset += (size - ret);
+
+	return (size - ret);
 }
-static ssize_t node_write(struct file *file, const char __user *buffer, size_t count, loff_t *offset)
+
+/* offset 实际是本地使用的，用于控制从哪里读取，写到哪里去 */
+static ssize_t node_write(struct file *file, const char __user *buffer, size_t usize, loff_t *offset)
 {
+	pr_info("%s %d: usize %ld offset %lld\n", __func__, __LINE__, usize, *offset);
+
+#if 1
+	if (usize > 0)
+		return usize;
+#else
+	/* 返回值小于写入的字节数，用户态会重复执行写入，直到完全写入成功 */
+	if (usize > 0)
+		return usize-1;
+#endif
+
 	return 0;
 }
 
